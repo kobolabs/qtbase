a515130c65dde3794c4ed87b7280fa32c74367a4
George Talusan <george.talusan@gmail.com>
2014-07-16 14:38:13 -0400

    expose backing image

--- src/platformsupport/fbconvenience/qfbscreen_p.h
+++ src/platformsupport/fbconvenience/qfbscreen_p.h
@@ -82,6 +82,8 @@ public:
     void scheduleUpdate();
     QRect mapToDevice(const QRect &rect) const;
 
+    const QImage *image() const { return mScreenImage; }
+
 public slots:
     virtual void setDirty(const QRect &rect, uint flags = 0);
     void setPhysicalSize(const QSize &size);
