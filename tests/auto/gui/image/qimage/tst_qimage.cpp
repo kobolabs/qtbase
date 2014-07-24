93ee0544a8954dc12a19bb9741fa55d1c3384ec3
Allan Sandfeld Jensen <allan.jensen@digia.com>
2014-07-24 15:54:58 +0200

    Ensure valid data after QImage::invertPixels
    QImage::invertPixels may produce invalid data after inversions of
    images with premultiplied alpha, because the inverted colors will be
    larger than the alpha.
    
    This patch converts any image with a premultiplied alpha channel to
    ARGB32 before inverting the pixels, and then back to the original
    format after the inversion.
    
    Support is added for correct inversion of RGBA8888 and RGB30 formats.
    
    Task-number: QTBUG-39901
    Change-Id: Ief24c55f495e67ef2ad6429b5b418d02963a64dd
    Reviewed-by: Gunnar Sletta <gunnar.sletta@jollamobile.com>

--- tests/auto/gui/image/qimage/tst_qimage.cpp
+++ tests/auto/gui/image/qimage/tst_qimage.cpp
@@ -152,6 +152,9 @@ private slots:
     void deepCopyWhenPaintingActive();
     void scaled_QTBUG19157();
 
+    void invertPixelsRGB_data();
+    void invertPixelsRGB();
+
     void cleanupFunctions();
 };
 
@@ -2125,6 +2128,39 @@ void tst_QImage::scaled_QTBUG19157()
     QImage foo(5000, 1, QImage::Format_RGB32);
     foo = foo.scaled(1024, 1024, Qt::KeepAspectRatio);
     QVERIFY(!foo.isNull());
+}
+
+void tst_QImage::invertPixelsRGB_data()
+{
+    QTest::addColumn<QImage::Format>("image_format");
+
+    QTest::newRow("invertPixels RGB16") << QImage::Format_RGB16;
+    QTest::newRow("invertPixels RGB32") << QImage::Format_RGB32;
+    QTest::newRow("invertPixels RGB444") << QImage::Format_RGB444;
+    QTest::newRow("invertPixels RGB555") << QImage::Format_RGB555;
+    QTest::newRow("invertPixels RGB888") << QImage::Format_RGB888;
+
+    QTest::newRow("invertPixels ARGB32") << QImage::Format_ARGB32;
+    QTest::newRow("invertPixels ARGB32pm") << QImage::Format_ARGB32_Premultiplied;
+    QTest::newRow("invertPixels RGBA8888") << QImage::Format_RGBA8888;
+    QTest::newRow("invertPixels RGBA8888pm") << QImage::Format_RGBA8888_Premultiplied;
+    QTest::newRow("invertPixels RGBA4444pm") << QImage::Format_ARGB4444_Premultiplied;
+}
+
+void tst_QImage::invertPixelsRGB()
+{
+    QFETCH(QImage::Format, image_format);
+
+    QImage image(1, 1, image_format);
+    image.fill(QColor::fromRgb(32, 64, 96));
+    image.invertPixels();
+
+    QCOMPARE(image.format(), image_format);
+
+    uint pixel = image.pixel(0, 0);
+    QCOMPARE(qRed(pixel) >> 4, (255 - 32) >> 4);
+    QCOMPARE(qGreen(pixel) >> 4, (255 - 64) >> 4);
+    QCOMPARE(qBlue(pixel) >> 4, (255 - 96) >> 4);
 }
 
 static void cleanupFunction(void* info)
