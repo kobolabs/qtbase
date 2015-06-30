65c095863a544b3d5cc264be07bb2a20cb040ffe
Marc Mutz <marc.mutz@kdab.com>
2015-06-30 22:14:24 +0200

    QModelIndex: Fix a nullptr literal 0 that would look odd as nullptr
    By dropping it. Found while looking into
    
    Task-number: QTBUG-45291
    Change-Id: Ie7655d607244aa67219ade465525ec771c87ccf5
    Reviewed-by: Olivier Goffart (Woboq GmbH) <ogoffart@woboq.com>

--- src/corelib/itemmodels/qabstractitemmodel.h
+++ src/corelib/itemmodels/qabstractitemmodel.h
@@ -497,7 +497,7 @@ inline QVariant QModelIndex::data(int arole) const
 { return m ? m->data(*this, arole) : QVariant(); }
 
 inline Qt::ItemFlags QModelIndex::flags() const
-{ return m ? m->flags(*this) : Qt::ItemFlags(0); }
+{ return m ? m->flags(*this) : Qt::ItemFlags(); }
 
 inline uint qHash(const QModelIndex &index)
 { return uint((index.row() << 4) + index.column() + index.internalId()); }
