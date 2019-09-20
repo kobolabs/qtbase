88d03f581da921d1fc46625c39754b1c56004016
George Talusan <george.talusan@gmail.com>
2019-09-20 14:14:11 -0400

    icu64 build fixes for macos and arm kobo specs

--- config.tests/unix/gnu-libiconv/gnu-libiconv.cpp
+++ config.tests/unix/gnu-libiconv/gnu-libiconv.cpp
@@ -48,7 +48,7 @@ int main(int, char **)
 {
     iconv_t x = iconv_open("", "");
 
-    const char *inp;
+    char *inp;
     char *outp;
     size_t inbytes, outbytes;
     iconv(x, &inp, &inbytes, &outp, &outbytes);
